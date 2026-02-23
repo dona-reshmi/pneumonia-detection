d7116cea9e0b379b1dafed7a9c89e9e6a2834b01
dona-reshmi <donajdr2005@gmail.com>
2026-02-23 11:27:01 +0530

    app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf
 import numpy as np
+from PIL import Image
 
-# Load model
-model = load_model("pneumonia_model.h5")
+# Load TFLite model
+interpreter = tf.lite.Interpreter(model_path="pneumonia_model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 st.title("Pneumonia Detection App")
 
-uploaded_file = st.file_uploader("Upload Chest X-ray Image", type=["jpg", "png"])
-if uploaded_file:
-    img = image.load_img(uploaded_file, target_size=(150,150))
-    img_array = image.img_to_array(img)/255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    prediction = model.predict(img_array)
-    
+uploaded_file = st.file_uploader("Upload Chest X-ray", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).resize((150,150))
+    img_array = np.array(image)/255.0
+    img_array = np.expand_dims(img_array, axis=0).astype(np.float32)
+
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(output_details[0]['index'])
+
     if prediction[0][0] > 0.5:
-        st.write("Prediction: Pneumonia")
+        st.success("Prediction: Pneumonia")
     else:
-        st.write("Prediction: Healthy")
+        st.success("Prediction: Normal")
